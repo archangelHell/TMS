5afabdb33a42e7971339ec6ba97aa45a8d0b7af7
archangelHell <118906144+archangelHell@users.noreply.github.com>
2023-09-13 02:14:53 +0300

    task 3

---
+++ perviedesat.java
@@ -0,0 +1,8 @@
+public class perviedesat {
+    public static void main(String[] args) {
+        for (int i = 0; i > -50; i--) {
+            System.out.println(i);
+            i -= 4;
+        }
+    }
+}
