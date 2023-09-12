8fada5d6b44ea8c11202fb7b7f246558b11a6743
archangelHell <118906144+archangelHell@users.noreply.github.com>
2023-09-13 01:12:50 +0300

    task 3

---
+++ chetnoeilinet.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class chetnoeilinet {
+
+        public static void main(String[] args) {
+            Scanner num = new Scanner(System.in);
+            System.out.println("Write number:");
+            int number = num.nextInt();
+            if (number % 2 == 0) {
+                System.out.print("четное");
+            } else if (number % 1 == 0) {
+                System.out.print("не четное");
+            }
+        }
+    }
+
