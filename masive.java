d898fcb4482d4e136cd5ca353c2b2203942091a0
archangelHell <118906144+archangelHell@users.noreply.github.com>
2023-09-13 03:47:21 +0300

    task 4

---
+++ masive.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class masive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter array length: ");
+        int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем
+        int[] array = new int[size];
+        System.out.println("Insert array elements:");
+        for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt();// Заполняем массив элементами
+        }
+        System.out.println("Enter needed massive: ");
+        int massive = input.nextInt();
+        boolean mas = Arrays.stream(array).anyMatch(i -> i == massive); //проверка
+        System.out.println(mas); //вывод false или true
+    }
+}
