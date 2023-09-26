d29fc1d707a274984746f7d5cad95d10939a388d
archangelHell <118906144+archangelHell@users.noreply.github.com>
2023-09-27 00:24:36 +0300

    task 5

---
+++ hw5t1.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class hw5t1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] RArray = new int[3][3];
+
+        System.out.println("Insert array elements:");
+        for (int i = 0; i < RArray.length; i++) {
+            RArray[i] = new int[]{sc.nextInt()};// Заполняем массив элементами
+        }
+        for (int j = 0; j < RArray.length; j++) {
+            RArray[j] = new int[]{sc.nextInt()};;// Заполняем массив элементами
+
+        }
+        System.out.println(Arrays.deepToString(RArray));
+                }
+            }
+
+
+
+
