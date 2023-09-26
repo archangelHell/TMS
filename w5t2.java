6739edc8cb279b46c0f52228caf19af09ab17714
archangelHell <118906144+archangelHell@users.noreply.github.com>
2023-09-27 01:26:33 +0300

    task 5

---
+++ w5t2.java
@@ -0,0 +1,20 @@
+public class w5t2 {
+    public static void main(String[] args) {
+        String[][] array = new String[8][8];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if ((i+j)% 2 == 0) {
+                    array[i][j] = "W";
+                }else{
+                    array[i][j]= "B";
+                }
+
+                System.out.print(" " + array[i][j]);
+
+            }
+            System.out.println();
+        }
+
+    }
+}
