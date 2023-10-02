2d78672793a1f2ee6062c165bf8d5423b6f3501b
archangelHell <118906144+archangelHell@users.noreply.github.com>
2023-10-02 22:13:22 +0300

    task 5

---
+++ w5t3.java
@@ -0,0 +1,26 @@
+public class w5t3 {
+    public static void main(String[]args){
+        Integer[][] arrA = {{1,0,0,0},{0,1,0,0},{0,0,0,0}};
+        Integer[][] arrB = {{1,2,3},{1,1,1},{0,0,0},{2,1,0}};
+        int a = arrA.length;
+        int b = arrB[0].length;
+        int o = arrB.length;
+        int[][] res = new int[a][b];
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                for (int k = 0; k < o; k++) {
+                    res[i][j] += arrA[i][k] * arrB[k][j];
+                }
+            }
+        }
+        for (int[] re : res) {
+            for (int j = 0; j < res[0].length; j++) {
+                System.out.format("%6d ", re[j]);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
