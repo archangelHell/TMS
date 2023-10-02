15105b1b984d3d751d4ea48cadd077d1bfb47dca
archangelHell <118906144+archangelHell@users.noreply.github.com>
2023-10-02 22:25:43 +0300

    task 5

---
+++ w5t4.java
@@ -0,0 +1,11 @@
+public class w5t4 {
+    public static void main(String[] args) {
+        int[] array = {3, 12, 6, 8};
+        int sum = 0;
+
+        for (int j : array) sum += j;
+        System.out.println(" summ =" + sum);
+
+        }
+    }
+
